d379443892a56ba221c7d39510181e7f93943073
iziplayz <58202306+iziplayz@users.noreply.github.com>
2019-11-26 19:40:58 +0400

    Update test.py

--- test.py
+++ test.py
@@ -48,3 +48,92 @@ print(high)
 '''
 # for j in i
 # print(j)
+
+
+
+
+'''
+import cards
+''' The basic process is this:
+    1) You create a Deck instance, which is filled (automatically) with 52 Card instances
+    2) You can deal those cards out of the deck into hands, each hand a list of cards
+    3) You then manipulate cards as you add/remove them from a hand
+'''
+value = 0
+my_dict = {}
+my_deck = cards.Deck()
+
+LocalDeck = str(my_deck)
+LocalDeck = LocalDeck.split(',')
+
+for i in LocalDeck:
+    my_dict[i] = value
+    value = (value+1)%13
+print(my_dict)
+    
+
+my_deck = cards.Deck()
+my_deck.shuffle()
+
+
+a_card = my_deck.deal()
+
+
+
+print("Dealt card is:",a_card)
+print('How many cards left:',my_deck.cards_count())
+
+print("Is the deck empty?",my_deck.is_empty())
+
+# deal some hands and print
+Community_cards=['9h','8c','7d','10h','8s'] 
+Player1_list=['5h','6s']
+Player2_list=['7a', 'Qd']
+
+def isStraight(community,player):
+    straight = []
+    
+    value_list = []
+    
+    alist = []
+    
+    straight.append(str(player[0]))
+    straight.append(str(player[1]))
+    for i in Community_cards:
+        straight.append(str(i))
+    print(straight)
+
+    
+    for i in straight:
+        find_value = my_dict.get(str(i))
+        print(i)
+        value_list.append(find_value)
+        LL = value_list
+        
+        value_list.sort()
+    
+    alist = list(set(value_list))
+    
+
+    if len(alist)>4:
+
+        if  (alist[4] - alist[0]) == 4:
+            for i in alist:
+                if LL[0] == i or LL[0] == i:
+                    return True
+        else:
+                return False
+
+def main(community,player1,player2):
+    if isStraight(community,player1) == True and isStraight(community,player2) == True:
+        print("It is a tie with straight")
+    elif isStraight(community,player1) == True:
+        print("Player 1 wins")
+    elif isStraight(community,player2) == True:
+        print("Player 2 wins")
+
+
+
+main(Community_cards,Player1_list,Player2_list)
+
+'''
